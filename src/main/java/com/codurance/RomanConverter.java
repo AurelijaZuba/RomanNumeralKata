9e4b26cd6bbe4ea788d9ff1262a2904d964dd50b
AurelijaZuba <auryte00@hotmail.com>
2019-04-08 12:21:00 +0100

    renamed the array list

--- src/main/java/com/codurance/RomanConverter.java
+++ src/main/java/com/codurance/RomanConverter.java
@@ -2,10 +2,10 @@ package com.codurance;
 
 
 public class RomanConverter {
-    private final String[] numberList = {"I", "II", "III", "IV"};
+    private final String[] romanList = {"I", "II", "III", "IV"};
 
     public String convert(int number) {
 
-        return numberList[number -1];
+        return romanList[number -1];
     }
 }
