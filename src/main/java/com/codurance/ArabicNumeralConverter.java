d67b8b2808c5f42779fd1ce8867243338bd2144c
AurelijaZuba <auryte00@hotmail.com>
2019-04-08 16:09:20 +0100

    Refactoring convert method, removed if statement and returned hash map list

--- src/main/java/com/codurance/ArabicNumeralConverter.java
+++ src/main/java/com/codurance/ArabicNumeralConverter.java
@@ -1,12 +1,17 @@
 package com.codurance;
 
+import java.util.LinkedHashMap;
+
 public class ArabicNumeralConverter {
+    private LinkedHashMap<String, Integer> arabicList = new LinkedHashMap<>();
+
+    public ArabicNumeralConverter() {
+        arabicList.put("I", 1);
+        arabicList.put("II", 2);
+    }
+
     public int convert(String romanNumeral) {
-        var result = 1;
-        if(romanNumeral.length() > 1)
-        {
-            result += 1;
-        }
-        return result;
+        return arabicList.get(romanNumeral);
+
     }
 }
