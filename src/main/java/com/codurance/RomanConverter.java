a5f033acdfa595afc27f0e9cbc1a0d309d630d25
AurelijaZuba <auryte00@hotmail.com>
2019-04-08 12:47:50 +0100

    Refactoring roman V

--- src/main/java/com/codurance/RomanConverter.java
+++ src/main/java/com/codurance/RomanConverter.java
@@ -5,23 +5,20 @@ import java.util.HashMap;
 
 public class RomanConverter {
     private final HashMap<Integer, String> romanList = new HashMap();
-    ;
 
     public RomanConverter() {
-
         romanList.put(1, "I");
         romanList.put(4, "IV");
         romanList.put(5, "V");
-        romanList.put(6, "VI");
-        romanList.put(7, "VII");
-        romanList.put(8, "VIII");
     }
 
     public String convert(int number) {
-
         if(romanList.containsKey(number)){
-
             return romanList.get(number);
+        }
+
+        if(number > 5) {
+            return "V" + convert(number - 5);
         }
 
         return "I" + convert(number - 1);
